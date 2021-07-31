92f19feca5a6d6274016f97fd3cf9da161d2eab0
ffranco-a <ffrancoaparicio@gmail.com>
2021-07-31 16:29:04 -0300

    a fex tweaks here and there (thanks Mati Monas)

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Logo from '../img/logoHenry.png';
 import SearchBar from './SearchBar.jsx';
 import { Link, NavLink } from 'react-router-dom';
 import style from '../components/Nav.module.css';
@@ -7,9 +6,9 @@ import style from '../components/Nav.module.css';
 function Nav({onSearch}) {
   return (
     <div className={style.navBar}>
-      <Link to='/'>
+      {/* <Link to='/'>
         <img src={Logo} alt="Logo Henry" />
-      </Link>
+      </Link> */}
       <Link to='/'>
         <span className={style.navButton}>Weather App</span>
       </Link>
--- src/containers/App.js
+++ src/containers/App.js
@@ -13,28 +13,33 @@ function App() {
   }
   function onSearch(ciudad) {
     //Llamado a la API del clima
+    if (ciudad === '') alert('Debes introducir una ciudad');
+    else 
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=4ae2636d8dfbdc3044bede63951a019b&units=metric`)
       .then(r => r.json())
       .then((recurso) => {
         if(recurso.main !== undefined){
-          const ciudad = {
-            min: Math.round(recurso.main.temp_min),
-            max: Math.round(recurso.main.temp_max),
-            img: recurso.weather[0].icon,
-            id: recurso.id,
-            wind: recurso.wind.speed,
-            temp: Math.round(recurso.main.temp),
-            name: recurso.name,
-            weather: recurso.weather[0].main,
-            weather2: recurso.weather[0].description,
-            clouds: recurso.clouds.all,
-            latitud: recurso.coord.lat,
-            longitud: recurso.coord.lon,
-            humedad: recurso.main.humidity,
-            sensacionTermica: Math.round(recurso.main.feels_like),
-            pais: recurso.sys.country
-          };
-          setCities(oldCities => [...oldCities, ciudad]);
+          if (cities.some((elem) => elem.name === recurso.name)) alert(`¡Ya hay tarjeta de ${recurso.name}!`);
+          else {
+            const ciudad = {
+              min: Math.round(recurso.main.temp_min),
+              max: Math.round(recurso.main.temp_max),
+              img: recurso.weather[0].icon,
+              id: recurso.id,
+              wind: recurso.wind.speed,
+              temp: Math.round(recurso.main.temp),
+              name: recurso.name,
+              weather: recurso.weather[0].main,
+              weather2: recurso.weather[0].description,
+              clouds: recurso.clouds.all,
+              latitud: recurso.coord.lat,
+              longitud: recurso.coord.lon,
+              humedad: recurso.main.humidity,
+              sensacionTermica: Math.round(recurso.main.feels_like),
+              pais: recurso.sys.country
+            };
+            setCities(oldCities => [...oldCities, ciudad]);
+          }
         } else {
           alert("Ciudad no encontrada");
         }
